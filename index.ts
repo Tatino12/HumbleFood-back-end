de2a79a67d9e8201e1517be44bf635d1a9d76070
ethanialq <ethanialq@gmail.com>
2022-03-03 10:57:41 -0600

    servidor - programación funcional

--- index.ts
+++ index.ts
@@ -1,37 +1,37 @@
-import { PrismaClient } from "@prisma/client";
-import express, { Router } from "express";
-import routes from "./src/routes";
+//import { PrismaClient } from "@prisma/client";
+import express from "express";
+//import routes from "./src/routes";
+import {router } from './src/routes/index'
+
+
+// class Server {
+//   app: express.Application;
+
+//   constructor() {
+//     this.app = express()
+//     this.config()
+//     this.routes()
+//   }
+
+//   config() {
+//     this.app.use(express.json())
+//   }
+//   routes(){
+//     this.app.use("/", routes)
+//   }
+
+//   start() {
+//     this.app.listen(3000, () => {
+//       console.log("servidor en el puerto 3000")
+//     })
+//   }
+// }
 
 
 
-class Server {
-  app: express.Application;
-
-  constructor() {
-    this.app = express()
-    this.config()
-    this.routes()
-  }
-
-  config() {
-    this.app.use(express.json())
-  }
-  routes(){
-    this.app.use("/", routes)
-  }
-
-  start() {
-    this.app.listen(3000, () => {
-      console.log("servidor en el puerto 3000")
-    })
-  }
-}
-
-
-
-const prisma = new PrismaClient()
-async function main() {
-    await prisma.$connect()
+// const prisma = new PrismaClient()
+// async function main() {
+//     await prisma.$connect()
   //   await prisma.users.create({
   //  data:  {
   //    name : "pikachu2",
@@ -51,18 +51,32 @@ async function main() {
   //   }
   //  }  
 //  })
-const allUsers = await prisma.users.findMany()
-console.log(allUsers)
-}
+// const allUsers = await prisma.users.findMany()
+// console.log(allUsers)
+// }
 
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+// main()
+//   .catch((e) => {
+//     throw e
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
 
 
-const server = new Server()
-server.start()
+// const server = new Server()
+// server.start()
+
+
+const server = express()
+
+// midlewares
+server.use(express.json())
+
+// routes
+server.use('/',router)
+
+server.listen(3000, () => {
+  console.log("servidor en el puerto 3000")
+})
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,38 +2,41 @@
 import { Request, Response } from "express";
 import { Router } from "express";
 import { Products } from "../controllers";
+import { User } from '../Items/Items'
+import { PrismaClient } from "@prisma/client"
 
-const router = Router()
+export const router = Router()
+const prisma: PrismaClient = new PrismaClient()
+
+router.get('/', async (req: Request, res: Response) => {
+    await prisma.$connect()
+    const users = await prisma.users.findMany()
+    console.log(users)
+    res.json(users)
+})
+
+// class Routes {
+//     router : Router
+//     products: Products
+//     constructor(){
+//         this.router = Router()
+//         this.products = new Products()
+//         this.routes()
+
+//     }
+//    async getProducts(req: Request, res: Response){
+//        const allProducts = await this.products.getProducts()
+//         res.send(allProducts)
+//     }
+//     routes(){
+//         this.router.get("/", this.getProducts)
+//     }
+// }
+
+// const routes = new Routes()
+// routes.routes()
+// export default routes.router
 
 
 
-class Routes {
-    router : Router
-    products: Products
-    constructor(){
-        this.router = Router()
-        this.products = new Products()
-        this.routes()
-
-    }
-   async getProducts(req: Request, res: Response){
-       const allProducts = await this.products.getProducts()
-        res.send(allProducts)
-    }
-    routes(){
-        this.router.get("/", this.getProducts)
-    }
-}
-
-const routes = new Routes()
-routes.routes()
-export default routes.router
-
-
-
-
-
-
-
-
-module.exports = router
+// module.exports = router
