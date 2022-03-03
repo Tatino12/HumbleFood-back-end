8d53504501c6ebe5e0560954302eb55058fcae25
Silvia Alvarez <silvia@MacBook-Air-de-Silvia.local>
2022-03-03 16:18:36 -0300

    Storage

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,24 +1,20 @@
 // controllers
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-
-
-
-export class Products{
-    prisma: PrismaClient
-    constructor(){
-        this.prisma = new PrismaClient()
-        this.conection()
-        
-    }
-   async conection(){
-        await this.prisma.$connect()
-    }
-  async getProducts(){
-   const allProducts = await this.prisma.products.findMany()
-    return allProducts 
-   }
+export class Products {
+  prisma: PrismaClient;
+  constructor() {
+    this.prisma = new PrismaClient();
+    this.conection();
+  }
+  async conection() {
+    await this.prisma.$connect();
+  }
+  async getProducts() {
+    const allProducts = await this.prisma.products.findMany();
+    return allProducts;
+  }
 }
 
-const products = new Products()
+const products = new Products();
 export default products;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,37 +3,23 @@ import { Request, Response } from "express";
 import { Router } from "express";
 import { Products } from "../controllers";
 
-const router = Router()
-
-
-
 class Routes {
-    router : Router
-    products: Products
-    constructor(){
-        this.router = Router()
-        this.products = new Products()
-        this.routes()
-
-    }
-   async getProducts(req: Request, res: Response){
-       const allProducts = await this.products.getProducts()
-        res.send(allProducts)
-    }
-    routes(){
-        this.router.get("/", this.getProducts)
-    }
+  router: Router;
+  products: Products;
+  constructor() {
+    this.router = Router();
+    this.products = new Products();
+    this.routes();
+  }
+  async getProducts(req: Request, res: Response) {
+    const allProducts = await this.products.getProducts();
+    res.send(allProducts);
+  }
+  routes() {
+    this.router.get("/", this.getProducts);
+  }
 }
 
-const routes = new Routes()
-routes.routes()
-export default routes.router
-
-
-
-
-
-
-
-
-module.exports = router
+const routes = new Routes();
+routes.routes();
+export default routes.router;
