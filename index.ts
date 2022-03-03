2c4fbcf46e83b722b9a4a548fe4d65d400d07433
Silvia Alvarez <silvia@MacBook-Air-de-Silvia.local>
2022-03-02 22:50:27 -0300

    first commit

--- index.ts
+++ index.ts
@@ -1,41 +1,30 @@
 import { PrismaClient } from "@prisma/client";
- 
+
 interface Shops {
   name: string;
   direction: string;
   image: string;
   description: string;
 }
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 async function main() {
-    await prisma.$connect()
-    await prisma.shops.create({
-   data: {
-    name : "mariano",
-    direction : "lasdas 123",
-    image : "asd",
-    description : "lala",
-    products : {
-      create : [{
-        name : "papa",
-        image : "as",
-        description : "asd",
-        price : 2,
-        stock : 3,
-
-      }
-      ]
-    }
-  }
-})
-const allUsers = await prisma.shops.findMany()
-console.log(allUsers)
+  await prisma.$connect();
+  await prisma.shops.create({
+    data: {
+      name: "mariano",
+      direction: "lasdas 123",
+      image: "asd",
+      description: "lala",
+    },
+  });
+  const allUsers = await prisma.shops.findMany();
+  console.log(allUsers);
 }
 
 main()
   .catch((e) => {
-    throw e
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
