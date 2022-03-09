98343a184fdc3f5ed3089be109d746c1c7a1d8ad
Silvia Alvarez <silvia@MacBook-Air-de-Silvia.local>
2022-03-09 15:18:47 -0300

    latest PR

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -3,39 +3,37 @@ import { Review } from "../Items/Review.interface";
 import prisma from "../database/db";
 import { resolveTxt } from "dns";
 
-
-
 export const addNewComment = async (data: any) => {
-   try {
-    const { pointProduct: PointProduct, ...rest } = data;  
+  try {
+    const { pointProduct: PointProduct, ...rest } = data;
     const prod = await prisma.products.findUnique({
-        where: {
-            id: rest.productId,
-        },
-        select: { pointProduct: true}
-    })
-
-    const newComment : any = await prisma.reviews.create({ data: rest})
-    const arrComment = await prisma.reviews.findMany({
-        where:{
-           productId: rest.productId, 
-        }
-    })
-    console.log(arrComment.length)
-    console.log(prod?.pointProduct)
-    const product = await prisma.products.update({ 
-        where: {
+      where: {
         id: rest.productId,
-    },
-    data: {
-        pointProduct: Math.ceil((prod?.pointProduct + PointProduct ) / arrComment.length)
-    }
-}) 
-console.log(prod)
-       return newComment;
-   } catch (error) {
-       return null;
-   } 
+      },
+      select: { pointProduct: true },
+    });
+
+    const newComment: any = await prisma.reviews.create({ data: rest });
+    const arrComment = await prisma.reviews.findMany({
+      where: {
+        productId: rest.productId,
+      },
+    });
+    console.log(arrComment.length);
+    console.log(prod?.pointProduct);
+    const product = await prisma.products.update({
+      where: {
+        id: rest.productId,
+      },
+      data: {
+        pointProduct: Math.ceil(
+          (prod?.pointProduct + PointProduct) / arrComment.length
+        ),
+      },
+    });
+    console.log(prod);
+    return newComment;
+  } catch (error) {
+    return null;
+  }
 };
-
-
