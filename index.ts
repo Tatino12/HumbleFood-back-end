9a038484401d794f5c008b19ce18867fdcc0bfce
Luciano <lucianobenavides96@hotmail.com>
2022-03-15 19:25:14 -0300

    installation

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { router } from "./src/routes/routes";
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3002;
 
@@ -8,9 +8,9 @@ const server = express();
 
 // midlewares
 
-server.use(express.json({ limit: '50mb' }));
-server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-server.use(morgan('dev'));
+server.use(express.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: true, limit: "50mb" }));
+server.use(morgan("dev"));
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
